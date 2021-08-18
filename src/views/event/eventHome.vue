46f6be703cf3906693f2a97fa3e99ea50a154d25
Cakpo crispus <cakpocrispus27@gmail.com>
2021-08-18 09:00:49 +0100

    axios

--- src/views/event/eventHome.vue
+++ src/views/event/eventHome.vue
@@ -379,18 +379,133 @@ export default {
     }
 
     const fetchTasks = () => {
-      store
-        .dispatch('app-todo/fetchTasks', {
-          q: searchQuery.value,
-          filter: router.currentRoute.params.filter,
-          tag: router.currentRoute.params.tag,
-          sortBy: sortBy.value,
-        })
-        .then(response => {
           console.log('rrrrrrrrrrrr')
-          console.log(response.data)
-          tasks.value = response.data
-        })
+          const datae = {
+  tasks: [
+    {
+      id: 1,
+      title: 'Entire change break our wife wide it daughter mention member.',
+      dueDate: '2020-11-25',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Jacob Ramirez',
+        avatar: require('@/assets/images/avatars/12.png'),
+      },
+      tags: ['update'],
+      isCompleted: false,
+      isDeleted: false,
+      isImportant: false,
+    },
+    {
+      id: 2,
+      title: 'Citizen stand administration step agency century.',
+      dueDate: '2020-12-14',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Andrew Anderson',
+        avatar: '',
+      },
+      tags: ['team', 'medium'],
+      isCompleted: true,
+      isDeleted: false,
+      isImportant: false,
+    },
+    {
+      id: 3,
+      title: 'Meet Jane and ask for coffee ❤️',
+      dueDate: '2020-11-25',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Benjamin Jacobs',
+        avatar: '',
+      },
+      tags: ['high'],
+      isCompleted: false,
+      isDeleted: false,
+      isImportant: false,
+    },
+    {
+      id: 4,
+      title: 'Answer the support tickets and close completed tickets. ',
+      dueDate: '2020-11-20',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Curtis Schmidt',
+        avatar: require('@/assets/images/avatars/9.png'),
+      },
+      tags: ['medium'],
+      isCompleted: false,
+      isDeleted: false,
+      isImportant: true,
+    },
+    {
+      id: 5,
+      title: 'Test functionality of apps developed by dev team for enhancements. ',
+      dueDate: '2020-12-06',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Katherine Perkins',
+        avatar: require('@/assets/images/avatars/9.png'),
+      },
+      tags: ['medium'],
+      isCompleted: true,
+      isDeleted: false,
+      isImportant: true,
+    },
+    {
+      id: 6,
+      title: 'Conduct a mini awareness meeting regarding health care. ',
+      dueDate: '2020-12-06',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'David Murphy',
+        avatar: '',
+      },
+      tags: ['high', 'medium'],
+      isCompleted: true,
+      isDeleted: true,
+      isImportant: false,
+    },
+    {
+      id: 7,
+      title: 'Plan new dashboard design with design team for Google app store. ',
+      dueDate: '2020-12-05',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Karina Miller',
+        avatar: require('@/assets/images/avatars/1.png'),
+      },
+      tags: ['medium'],
+      isCompleted: false,
+      isDeleted: false,
+      isImportant: true,
+    },
+    {
+      id: 8,
+      title: 'Pick up Nats from her school and drop at dance class😁 ',
+      dueDate: '2020-12-08',
+      description:
+        '<p>Chocolate cake topping bonbon jujubes donut sweet wafer. Marzipan gingerbread powder brownie bear claw. Chocolate bonbon sesame snaps jelly caramels oat cake.</p>',
+      assignee: {
+        fullName: 'Thomas Moses',
+        avatar: require('@/assets/images/avatars/7.png'),
+      },
+      tags: ['low', 'medium'],
+      isCompleted: false,
+      isDeleted: false,
+      isImportant: false,
+    },
+ 
+  ],
+}
+          tasks.value = datae
     }
 
     fetchTasks()
